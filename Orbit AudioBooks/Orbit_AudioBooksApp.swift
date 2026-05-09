cc579dc3386e56183f07bfd0ffe282a7b9af873f
Dan Fakkeldy <2112591+dfakkeldy@users.noreply.github.com>
2026-05-09 14:59:27 -0300

    Adding macOS app

---
+++ Orbit AudioBooks/Orbit_AudioBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Orbit_AudioBooksApp.swift
+//  Orbit AudioBooks
+//
+//  Created by Dan Fakkeldy on 2026-05-09.
+//
+
+import SwiftUI
+
+@main
+struct Orbit_AudioBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
